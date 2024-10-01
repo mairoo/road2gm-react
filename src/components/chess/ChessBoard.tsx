6905fc2ad8d71536ce0198814bed60fb2159e959
jonghwa <jonghwa@pincoin.co.kr>
2024-10-02 00:36:48 +0900

    coordinate display

--- src/components/chess/ChessBoard.tsx
+++ src/components/chess/ChessBoard.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Square from "./Square";
+import {
+  MdArrowBack,
+  MdArrowForward,
+  MdPlayArrow,
+  MdSkipNext,
+  MdSkipPrevious,
+} from "react-icons/md";
 
 const ChessBoard = () => {
   // 좌표 순서 일차원 배열: a8, b8, c8, ... f1, g1, h1
@@ -11,14 +18,66 @@ const ChessBoard = () => {
     FILES.map((file) => ({ file, rank })),
   ).flat();
 
+  const [coordinate, setCoordinate] = useState("");
+
   return (
-    <div className="mx-auto max-w-xl sm:px-6 lg:px-8 relative">
-      <div className="bg-[url('https://road2gm.co.kr/assets/chess/boards/blue.svg')] bg-no-repeat bg-cover">
+    <div className="mx-auto max-w-xl sm:px-6 lg:px-8">
+      <div className="bg-[url('https://road2gm.co.kr/assets/chess/boards/blue.svg')] bg-no-repeat bg-cover relative">
         <div className="grid grid-cols-8">
           {COORDINATES.map((square) => (
-            <Square file={square.file} rank={square.rank}></Square>
+            <Square
+              file={square.file}
+              rank={square.rank}
+              key={square.file + square.rank}
+              onClick={() => {
+                setCoordinate(square.file + square.rank);
+              }}
+            ></Square>
           ))}
         </div>
+      </div>
+      <div className="mt-2">
+        <span className="isolate inline-flex rounded-md shadow-sm">
+          <button
+            type="button"
+            className="relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          >
+            <MdSkipPrevious />
+          </button>
+          <button
+            type="button"
+            className="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          >
+            <MdArrowBack />
+          </button>
+          <button
+            type="button"
+            className="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          >
+            <MdPlayArrow />
+          </button>
+          <button
+            type="button"
+            className="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          >
+            <MdArrowForward />
+          </button>
+          <button
+            type="button"
+            className="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          >
+            <MdSkipNext />
+          </button>
+          <button
+            type="button"
+            className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          >
+            flip
+          </button>
+        </span>
+      </div>
+      <div>
+        <input value={coordinate} readOnly={true} />
       </div>
     </div>
   );
--- src/components/chess/Square.tsx
+++ src/components/chess/Square.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { ComponentPropsWithoutRef } from "react";
 
-const Square = ({ file, rank }: { file: string; rank: number }) => {
+const Square = ({
+  file,
+  rank,
+  onClick,
+}: {
+  file: string;
+  rank: number;
+  onClick: () => void;
+} & ComponentPropsWithoutRef<"div">) => {
   return (
-    <div className="aspect-square text-gray-100 " key={file + rank}>
+    <div className="aspect-square text-gray-100" onClick={onClick}>
       {file}
       {rank}
     </div>
