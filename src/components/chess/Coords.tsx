40f74a79c44266f8f0186b56c4f4e2ff8dbfee89
jonghwa <jonghwa@pincoin.co.kr>
2024-10-04 14:20:33 +0900

    coords loop

---
+++ src/components/chess/Coords.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Coords = () => {
+  return (
+    <svg viewBox="0 0 100 100">
+      {[1, 2, 3, 4, 5, 6, 7, 8].map((rank, index) => {
+        return (
+          <text
+            x="0.5"
+            y={3 + index * 12.5}
+            fontSize="2.8"
+            fill={index % 2 === 0 ? "#999" : "#eee"}
+            key={rank}
+          >
+            {rank}
+          </text>
+        );
+      })}
+      {["h", "g", "f", "e", "d", "c", "b", "a"].map((file, index) => {
+        return (
+          <text
+            x={10 + index * 12.5}
+            y="99"
+            fontSize="2.8"
+            fill={index % 2 === 0 ? "#eee" : "#999"}
+            key={file}
+          >
+            {file}
+          </text>
+        );
+      })}
+    </svg>
+  );
+};
+
+export default Coords;
