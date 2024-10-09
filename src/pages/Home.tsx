6a93e20b55b81d2b0eacfd4825e58f61baf7fc1b
jonghwa <jonghwa@pincoin.co.kr>
2024-10-10 01:07:04 +0900

    make a move

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,35 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
+import _ from "lodash";
 
 const Home = () => {
+  const [game, setGame] = useState(new Chess());
+
+  const onDrop = (
+    sourceSquare: string,
+    targetSquare: string,
+    piece: string,
+  ) => {
+    // 객체 복사
+    const gameCopy = _.cloneDeep(game);
+
+    // 수를 두기
+    try {
+      gameCopy.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: piece[1].toLowerCase() ?? "q",
+      });
+    } catch (error) {
+      console.debug(error);
+      return false;
+    } finally {
+      setGame(gameCopy);
+    }
+    return true;
+  };
+
   const pieces = [
     "wP",
     "wN",
@@ -36,20 +64,25 @@ const Home = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Road2GM</h1>
-      <Chessboard
-        boardOrientation="black"
-        customBoardStyle={{
-          borderRadius: "4px",
-          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.5)",
-        }}
-        customDarkSquareStyle={{
-          backgroundColor: "#779952",
-        }}
-        customLightSquareStyle={{
-          backgroundColor: "#edeed1",
-        }}
-        customPieces={customPieces}
-      />
+
+      <div className="md:w-96">
+        <Chessboard
+          boardOrientation="white"
+          position={game.fen()}
+          onPieceDrop={onDrop}
+          customBoardStyle={{
+            borderRadius: "4px",
+            boxShadow: "0 2px 10px rgba(0, 0, 0, 0.5)",
+          }}
+          customDarkSquareStyle={{
+            backgroundColor: "#779952",
+          }}
+          customLightSquareStyle={{
+            backgroundColor: "#edeed1",
+          }}
+          customPieces={customPieces}
+        />
+      </div>
     </>
   );
 };
