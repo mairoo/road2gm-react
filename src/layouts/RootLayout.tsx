5eda6d939dbe2eb55a86ace3ec7e3a17c02d576d
jonghwa <jonghwa@pincoin.co.kr>
2024-10-19 00:33:27 +0900

    min-h-dvh

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -55,12 +55,12 @@ const RootLayout = () => {
 
   // 헤더, 본문, 푸터 3단 레이아웃
   // flex flex-col: 세로 방향 flex 컨테이너
-  // min-h-screen: 최소 높이를 화면 전체 높이로 지정
+  // min-h-dvh: 최소 높이를 화면 전체 높이로 지정
   // 헤더: 페이지 상단 위치
   // 본문: 내용이 많아지면 전체 화면으로 스크롤 생성 (중요! flex-grow: 남은 공간을 모두 차지)
   // 푸터: 내용이 적을 때는 화면 하단 고정, 내용이 많아 스크롤이 생기면 스크롤의 맨 아래에 위치
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-dvh">
       <Header>
         <div className="bg-teal-50 py-2 px-2 md:px-0">
           <ContainerFixed>
