4da5974ac19a580c1d3dec197f666ecae8414919
jonghwa <jonghwa@pincoin.co.kr>
2024-11-08 00:47:36 +0900

    organize code convention

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -289,6 +289,19 @@ const tocData = {
 };
 
 const BookDetailPage = () => {
+  // 1. react-router-dom 훅
+  // 2. Redux 훅
+  // 3. RTK Query 훅
+  // 4. useState 훅
+  // 5. useRef 훅
+  // 6. useMemo 훅
+  // 7. useEffect 훅
+  // 8. 페이지 이동 네비게이션 핸들러 useCallback 훅
+  // 9. 이벤트 핸들러 useCallback 훅
+  // 10. 헬퍼 함수
+  // 11. 렌더 메소드 (renderForm, renderError, renderList 등)
+  // 12. 메인 컴포넌트 렌더링 반환
+
   const { id } = useBookParams();
 
   const currentSectionId = 25;
