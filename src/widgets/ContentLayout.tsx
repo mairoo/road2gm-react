d665df512e2be1c41450faadffdd7f69862d5cfe
jonghwa <jonghwa@pincoin.co.kr>
2024-11-10 12:10:54 +0900

    remove unnecessary options

--- src/widgets/ContentLayout.tsx
+++ src/widgets/ContentLayout.tsx
@@ -3,8 +3,6 @@ import React, { ReactNode } from "react";
 
 type LayoutPosition = "center" | "top" | "top-sm" | "top-lg";
 type Align = "left" | "center" | "right";
-type Direction = "row" | "col";
-type Gap = "none" | "sm" | "md" | "lg";
 
 interface LayoutProps {
   children: ReactNode;
@@ -12,12 +10,6 @@ interface LayoutProps {
   align?: Align;
   className?: string;
   width?: "full" | "1/2" | "1/3" | "2/3" | "1/4" | "3/4";
-  padding?: boolean;
-  direction?: Direction;
-  gap?: Gap;
-  wrap?: boolean;
-  grow?: boolean;
-  stretch?: boolean;
 }
 
 const ContentLayout = ({
@@ -26,19 +18,9 @@ const ContentLayout = ({
   align = "left",
   className: additionalClassName = "",
   width = "1/2",
-  padding = true,
-  direction = "col",
-  gap = "none",
-  wrap = false,
-  grow = false,
-  stretch = false,
 }: LayoutProps) => {
   const containerClasses = className(
-    "flex",
-    {
-      "flex-col": direction === "col",
-      "flex-row": direction === "row",
-    },
+    "flex flex-col",
     {
       "py-4 px-2 md:pt-12 md:px-0": position === "top-sm",
       "py-4 px-2 md:pt-32 md:px-0": position === "top",
@@ -53,22 +35,7 @@ const ContentLayout = ({
   );
 
   const contentClasses = className(
-    "flex",
-    {
-      "flex-col": direction === "col",
-      "flex-row": direction === "row",
-      "flex-wrap": wrap,
-      "flex-nowrap": !wrap,
-      "flex-grow": grow,
-      "h-full": stretch,
-    },
-    {
-      "gap-0": gap === "none",
-      "gap-2": gap === "sm",
-      "gap-4": gap === "md",
-      "gap-6": gap === "lg",
-    },
-    { "p-4": padding },
+    "flex flex-col",
     {
       "w-full": width === "full",
       "w-full md:w-1/2": width === "1/2",
